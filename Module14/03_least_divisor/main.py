14e3088371ec9ec7af370084c3dc1cf1246f4897
EugenyOvchinnikov <eugeny.v.ovchinnikov@gmail.com>
2023-03-21 22:59:19 +0300

    Задание выполнено

--- Module14/03_least_divisor/main.py
+++ Module14/03_least_divisor/main.py
@@ -1,1 +1,11 @@
-# TODO здесь писать код
+def get_minimal_divider(number):
+    minimal_divider = number
+    for i in range(number - 1, 1, -1):
+        if number % i == 0:
+            minimal_divider = i
+
+    return minimal_divider
+
+
+my_number = int(input('Введите число: '))
+print(f'Наименьший делитель, отличный от единицы: {get_minimal_divider(my_number)}')
