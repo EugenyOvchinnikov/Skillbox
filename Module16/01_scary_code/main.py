a131ba5f13a1745ebbb9f8ca69f30b6e51943d1a
EugenyOvchinnikov <eugeny.v.ovchinnikov@gmail.com>
2023-03-24 13:51:07 +0300

    Module16 task 01

--- Module16/01_scary_code/main.py
+++ Module16/01_scary_code/main.py
@@ -1,24 +1,16 @@
-a = [1, 5, 3]
-b = [1, 5, 1, 5]
-c = [1, 3, 1, 5, 3, 3]
-for i in b:
-    a.append(i)
-t = 0
-for i in a:
-    if i == 5:
-        t += 1
-print(t)
-d = []
-for i in a:
-    if i != 5:
-        d.append(i)
-for i in c:
-    d.append(i)
-t = 0
-for i in d:
-    if i == 3:
-        t += 1
-print(t)
-print(d)
+master_list = [1, 5, 3]
+first_slave_list = [1, 5, 1, 5]
+second_slave_list = [1, 3, 1, 5, 3, 3]
 
-# TODO переписать программу
+master_list.extend(first_slave_list)
+quantity_5 = master_list.count(5)
+
+for _ in range(quantity_5):
+    master_list.remove(5)
+
+master_list.extend(second_slave_list)
+quantity_3 = master_list.count(3)
+
+print('Количество цифр 5 при первом объединении: ', quantity_5)
+print('Количество цифр 3 при втором объединении: ', quantity_3)
+print('Итоговый список: ', master_list)
