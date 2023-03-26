393052edafbdeaf8f1ae9e3c620d23535e56c08f
EugenyOvchinnikov <eugeny.v.ovchinnikov@gmail.com>
2023-03-26 22:01:11 +0300

    06 07

--- Module17/06_two_dimensional_list/main.py
+++ Module17/06_two_dimensional_list/main.py
@@ -1,1 +1,5 @@
-# TODO здесь писать код
+step = 4
+
+user_list = [(list(range(i, i + step * 2 + 1, step))) for i in range(1, 5)]
+
+print(user_list)
--- Module17/07_list_of_lists_2/main.py
+++ Module17/07_list_of_lists_2/main.py
@@ -1,4 +1,13 @@
+# nice_list = [[[1, 2, 3], [4, 5, 6], [7, 8, 9]],
+#              [[10, 11, 12], [13, 14, 15], [16, 17, 18]]]
+#
+# res_list = [res_list.extend(x) for x in nice_list]
+#
+# print(res_list)
+
 nice_list = [[[1, 2, 3], [4, 5, 6], [7, 8, 9]],
              [[10, 11, 12], [13, 14, 15], [16, 17, 18]]]
 
-# TODO здесь писать код
+flat = [lvl_1 for lvl_1 in nice_list]
+# flat = [lvl_3 for lvl_1 in nice_list for lvl_2 in lvl_1 for lvl_3 in lvl_2]
+print(flat)
