72e5552dd6f4cc27a6771fd847b2bff392422a35
EugenyOvchinnikov <eugeny.v.ovchinnikov@gmail.com>
2023-03-27 17:36:15 +0300

    Module17

--- Module17/07_list_of_lists_2/main.py
+++ Module17/07_list_of_lists_2/main.py
@@ -1,13 +1,6 @@
-# nice_list = [[[1, 2, 3], [4, 5, 6], [7, 8, 9]],
-#              [[10, 11, 12], [13, 14, 15], [16, 17, 18]]]
-#
-# res_list = [res_list.extend(x) for x in nice_list]
-#
-# print(res_list)
-
 nice_list = [[[1, 2, 3], [4, 5, 6], [7, 8, 9]],
              [[10, 11, 12], [13, 14, 15], [16, 17, 18]]]
 
-flat = [lvl_1 for lvl_1 in nice_list]
-# flat = [lvl_3 for lvl_1 in nice_list for lvl_2 in lvl_1 for lvl_3 in lvl_2]
-print(flat)
+res_list = [step3 for step1 in nice_list for step2 in step1 for step3 in step2]
+
+print(res_list)
--- Module17/08_caesar_cipher/main.py
+++ Module17/08_caesar_cipher/main.py
@@ -1,1 +1,24 @@
-# TODO здесь писать код
+def get_index(my_sym, my_step, lst):
+    index = lst.index(my_sym)
+    if index + my_step > len(lst) - 1:
+        index = index + step - len(lst)
+    else:
+        index += step
+    return index
+
+
+text = input('Введите сообщение: ').lower()
+step = int(input('Введите сдвиг: '))
+
+alphabet = ['а', 'б', 'в', 'г', 'д', 'е', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о',
+            'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я']
+
+encrypted_text = ''
+
+for sym in text:
+    if sym in alphabet:
+        encrypted_text += alphabet[get_index(sym, step, alphabet)]
+    else:
+        encrypted_text += sym
+
+print(encrypted_text)
