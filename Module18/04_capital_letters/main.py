f6e214b70fc318f2b526c82ec872d65f78a05b46
EugenyOvchinnikov <eugeny.v.ovchinnikov@gmail.com>
2023-03-29 08:37:22 +0300

    Module18 complite

--- Module18/04_capital_letters/main.py
+++ Module18/04_capital_letters/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+text_line = str(input('Введите текст: ').title())
+
+# text_list = text_line.split()
+#
+# for index, word in enumerate(text_list):
+#     text_list[index] = word[0:1].upper() + word[1:]
+# text_line = ' '.join(text_list)
+
+print('Результат:', text_line)
