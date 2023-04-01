0bbd07595b1f64987aec99356380ad400fcab7f9
EugenyOvchinnikov <eugeny.v.ovchinnikov@gmail.com>
2023-04-02 01:05:32 +0300

    module19 complite

--- Module19/02_cryptocurrency/main.py
+++ Module19/02_cryptocurrency/main.py
@@ -44,5 +44,12 @@ data = {
     ]
 }
 
+for key in data:
+    print(key, ' -', data[key])
 
-# TODO здесь писать код
+data['ETH']['total_diff'] = 100
+data['tokens'][0]['fst_token_info']['name'] = 'doge'
+
+data['ETH']['totalOut'] = data['tokens'][0].pop('total_out') + data['tokens'][1].pop('total_out')
+
+data['tokens'][1]['sec_token_info']['total_price'] = data['tokens'][1]['sec_token_info'].pop('price')
