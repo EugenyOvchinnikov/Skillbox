f0df65a8bbb5c2bc8d5ec05c5db0a008437c7f02
EugenyOvchinnikov <eugeny.v.ovchinnikov@gmail.com>
2023-05-02 17:29:06 +0300

    03 added

--- Module25/03_my_dict/main.py
+++ Module25/03_my_dict/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+class MyDict(dict):
+    def get(self, key, default=None):
+        return super().get(key, 0)
+
+
+a = MyDict({'a': 1, 'b': 2})
+
+print(a.get('a'))
+print(a.get('e'))
