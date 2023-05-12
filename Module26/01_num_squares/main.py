a49cb09f6abeb9b9ddb7e208e06ff09be2be9ce8
EugenyOvchinnikov <eugeny.v.ovchinnikov@gmail.com>
2023-05-12 17:15:38 +0300

    02 complite

--- Module26/01_num_squares/main.py
+++ Module26/01_num_squares/main.py
@@ -1,1 +1,50 @@
-# TODO здесь писать код
+from collections.abc import Iterable
+
+
+class Square:
+    """ Класс итератор """
+    __current_number = 0
+
+    def __init__(self, number: int) -> None:
+        self.__number = number
+
+    def __iter__(self) -> Iterable[int]:
+        return self
+
+    def __next__(self) -> int:
+        Square.__current_number += 1
+        if Square.__current_number > self.__number:
+            raise StopIteration
+        else:
+            return Square.__current_number ** 2
+
+
+# функция-генератор
+def squares():
+    n = 1
+    while True:
+        yield n ** 2
+        n += 1
+
+
+if __name__ == '__main__':
+    my_number = int(input('Введите число N: '))
+
+    # Используем класс-итератор
+    my_iter = Square(my_number)
+
+    for i_elem in my_iter:
+        print(i_elem, end=' ')
+
+    # Используем функцию-генератор
+    print()
+    my_iter2 = squares()
+    for _ in range(my_number):
+        print(next(my_iter2), end=' ')
+
+    # Генераторное выражение
+    my_iter3 = (i ** 2 for i in range(1, my_number + 1))
+
+    print()
+    for i_elem in my_iter3:
+        print(i_elem, end=' ')
--- Module26/02_files_path/main.py
+++ Module26/02_files_path/main.py
@@ -1,1 +1,31 @@
-# TODO здесь писать код
+import os.path
+
+
+class FindFolder(Exception):
+    pass
+
+
+def gen_files_path(link: str, search: str):
+    for links, dirs, files in list(os.walk(link)):
+        for file in files:
+            if links.split('\\')[-1] == search:
+                raise FindFolder('каталог найден')
+            else:
+                yield links + '\\' + file
+
+
+if __name__ == '__main__':
+    # path_search = input("Введите путь для поиска: ")
+    path_search = 'C:\\Users\\john\\PycharmProjects'
+    # folder_search = input("Какой каталог будем искать? ")
+    folder_search = 'Module155'
+
+    my_iter = gen_files_path(path_search, folder_search)
+
+    try:
+        while True:
+            print(next(my_iter))
+    except FindFolder as exc:
+        print(exc)
+    except StopIteration:
+        print('каталог не найден')
